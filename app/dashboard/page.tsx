d0b914ef2989d9e54ae910994efd31526d8df551
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-25 20:59:31 +0330

    feat(dashboard): fetch whole cards data at once

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,9 +5,16 @@ import RevenueChart from '../ui/dashboard/revenue-chart';
 import { lusitana } from '../ui/fonts';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+  const [
+    revenue,
+    latestInvoices,
+    {
+      totalPaidInvoices,
+      totalPendingInvoices,
+      numberOfInvoices,
+      numberOfCustomers
+    }
+  ] = await Promise.all([fetchRevenue(), fetchLatestInvoices(), fetchCardData()]);
 
   return (
     <main>
