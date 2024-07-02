7956c3b838905d2b3a35d6996b17842c61d9f52a
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-07-02 17:58:18 +0330

    feat(dashboard): add invoice updating

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,6 +30,24 @@ export async function createInvoice(formData: FormData) {
 
   const invoicePath = '/dashboard/invoices';
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+ 
+  const invoicePath = '/dashboard/invoices';
   revalidatePath(invoicePath);
   redirect(invoicePath);
 }
